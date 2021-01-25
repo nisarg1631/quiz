207c15ef53263b53abbf3fdc8b2f292a5cb056f1
JhadotAnimesh <jha.animesh01@gmail.com>
2021-01-25 23:08:55 +0530

    added answer slide

--- app_src/views.py
+++ app_src/views.py
@@ -1,8 +1,9 @@
+
 from app_src import app
 from flask import render_template
 import json
 
-with open('./quiz_data/new_year.json') as f:
+with open('./quiz_data/index.json') as f:
   data = json.load(f)
 
 @app.route("/")
@@ -16,8 +17,17 @@ def about():
 @app.route("/question/<quiz_num>/<ques_num>")
 def ques(quiz_num,ques_num):
     print(quiz_num,ques_num)
-    print(data['Q1'])
-    return render_template("question.html",question_url=data["Q1"]["q_url"])
+    if(quiz_num in data): 
+        print(data[quiz_num])
+        with open(f'./quiz_data/{data[quiz_num]}') as f1:
+            lo_data = json.load(f1)
+        if(("Q"+ques_num ) in lo_data):
+            return render_template("question.html",question_url=lo_data["Q"+ques_num]["q_url"],answer_url=lo_data["Q"+ques_num]["a_url"])
+        else :
+            return "invalid"
+        
+    else:
+        return "invalid"
 
 @app.route("/questionview/<ques_num>")
 def quesview(ques_num):
