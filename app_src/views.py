a4d24573ebbfa1d18462c769a07c374a98a4b64a
JhadotAnimesh <jha.animesh01@gmail.com>
2021-01-25 21:57:10 +0530

    basic

--- app_src/views.py
+++ app_src/views.py
@@ -1,9 +1,20 @@
 from app_src import app
+from flask import render_template
+import json
+
+with open('/home/animesh/Documents/qc_ques_website/quiz/quiz_data/new_year.json') as f:
+  data = json.load(f)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return "Insert Welcome Page Here"
 
 @app.route("/about")
 def about():
-    return "All about Flask"
+    return "Insert About Page Here"
+
+@app.route("/question/<quiz_num>/<ques_num>")
+def ques(quiz_num,ques_num):
+    print(quiz_num,ques_num)
+    print(data['Q1'])
+    return render_template("question.html",quiz_dict=data["Q1"])
